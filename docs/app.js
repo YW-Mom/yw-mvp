64d8c51f16e5c1df261a3167e15f36ef04c3f9e7
YW-Mom <finewinedevil3@gmail.com>
2026-09-19 20:37:16 +0800

    Initialize Supabase client and load products
    Added Supabase client setup and product loading function.

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,34 @@
 
+const SUPABASE_URL = "https://fjdbwzyibriusxbilcaq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_t7xD8lY1WjHj9HiYoxCnbA_HRhYWuZV";
+
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function loadProducts(){
+
+  const { data, error } = await client
+    .from("products")
+    .select("*");
+
+  if(error){
+    console.log(error);
+    return;
+  }
+
+    document.getElementById("product-list").innerHTML =
+    data.map(product => `
+      <tr>
+        <td>${product.name}</td>
+        <td>${product.sku}</td>
+        <td>${product.cost}</td>
+        <td>${product.retail_price}</td>
+        <td>${product.reseller_price}</td>
+      </tr>
+    `).join("")
+
+}
+
+loadProducts();
